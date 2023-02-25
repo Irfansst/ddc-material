ca89c9fda279a44cc52d774ae8abddd70fcebca4
Irfansst <irfansjbcse@gmail.com>
2023-02-25 12:30:01 +0530

    test

---
+++ test..groovy
@@ -0,0 +1,20 @@
+List<String> projectNames = ["project1", "project2", "project3"]
+
+projectNames.each { name ->
+    job(name) {
+        description("name")
+			scm {
+				git("https://github.com/anshulc55/Jenkins_Upgradev3.git", 'master')
+			}
+    triggers {
+        scm('* * * * *')
+    }
+    steps {
+        maven('clean package', 'maven-samples/single-module/pom.xml')
+    }
+    publishers {
+        //archive the war file generated
+        archiveArtifacts '**/*.jar'
+    }
+    }
+}
