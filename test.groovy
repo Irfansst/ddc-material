436069f239c93d8907771deab019ede5e568d8e7
Irfansst <irfansjbcse@gmail.com>
2023-02-25 12:34:39 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -2,7 +2,7 @@ List<String> projectNames = ["project1", "project2", "project3"]
 
 projectNames.each { name ->
     job(name) {
-        description("name")
+        description(name)
 			scm {
 				git("https://github.com/anshulc55/Jenkins_Upgradev3.git", 'master')
 			}
