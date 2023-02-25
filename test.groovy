05c88d54c7e6d69378af744902019589e88b7b2c
Irfansst <irfansjbcse@gmail.com>
2023-02-25 12:39:49 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,22 +1,20 @@
-List<String> projectNames = ["project1", "project2", "project3"]
-listView("Release_7.0") {
+List<String> projectNames = ["project01", "project02", "project03"]
+
+listView("My Projects") {
     // add the three projects to the view
-projectNames.each { name ->
-    job(name) {
-        description(name)
-			scm {
-				git("https://github.com/anshulc55/Jenkins_Upgradev3.git", 'master')
-			}
-    triggers {
-        scm('* * * * *')
+    projectNames.each { name ->
+        job(name) {
+            // set the project description to be the same as the project name
+            description(name)
+        
+            // configure the job here
+            // for example:
+            scm {
+                git("https://github.com/myorg/myrepo.git")
+            }
+            steps {
+                shell("mvn clean install")
+            }
+        }
     }
-    steps {
-        maven('clean package', 'maven-samples/single-module/pom.xml')
-    }
-    publishers {
-        //archive the war file generated
-        archiveArtifacts '**/*.jar'
-    }
-    }
-}
 }
